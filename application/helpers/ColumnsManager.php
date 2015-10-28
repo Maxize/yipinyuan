903791af532147db8159c5a290a42b4312ef1e88
Amnon <hzmsmail.2@gmail.com>
2015-10-28 22:52:52 +0800

    generateTree
    fix the bug of generate the tree

--- application/helpers/ColumnsManager.php
+++ application/helpers/ColumnsManager.php
@@ -1,34 +1,16 @@
 <?php
 include "entity/Columns.php";
+
 class ColumnsManager {
 	var $columnsArrayList;
-
-	public function init($arrayData) 
-	{
-		$i = 0;
-		foreach ($arrayData as $columnData) {
-			$column = new Columns;
-			$column->setParentId($columnData['parentId']);
-			$column->setColumnsName($columnData['columnsName']);
-			$column->setColumnsId($columnData['columnsId']);
-			print_r($column);
-			$this->addItem($column);
-			$i++;
-		}
-	}
-
-	public function printColumnsArray() {
-		print_r($this->columnsArrayList);
-
-	}
 
 	public function getRoot() 
 	{
 		$columnItem = array();
 		foreach ($this->columnsArrayList as $column) {
-			$parentId = $column->getParentId();
+			$parentId = $column['parentId'];
 			if (0 == $parentId) {
-				$columnItem = $column;
+				$columnItem[] = $column;
 			}
 		}
 		return $columnItem;
@@ -38,9 +20,8 @@ class ColumnsManager {
 	{
 		$columnList = array();
 		$i = 0;
-		print_r($this->columnsArrayList);
 		foreach ($this->columnsArrayList as $column) {
-			if ($parentId == $column->getParentId()) {
+			if ($parentId == $column['parentId']) {
 				$columnList[$i] = $column;
 			}
 			$i++;
@@ -49,70 +30,23 @@ class ColumnsManager {
 	}
 
 	/**
-	 * [getTreeByParentId description]
-	 * @param  integer $id [default 0 父节点id]
-	 * @return [string]      [输出一颗str树]
+	 * 生成 树
+	 * @param  [type] $items [description]
+	 * @return [type]        [description]
 	 */
-	public function getTreeByParentId($id=0)
+	function generateTree($items)
 	{
-		$treeArray = array();
-		$treeArray = $this->get_array(0);
-		print_r($treeArray);
-	}
-
-
-	function generateTree($items){
 	    $tree = array();
 	    foreach($items as $item){
-	        if(isset($items[$item->getParentId()])){
-	        	$objArray = get_object_vars($items[$item->getColumnsId()]);
-	            $items[$item->getParentId()]['son'][] = $objArray;
+	        if(isset($items[$item['parentId']])){
+	            $items[$item['parentId']]['list'][] = &$items[$item['columnsId']];
 	        }else{
-	            $tree[] = &$items[$item->getColumnsId()];
+	            $tree[] = &$items[$item['columnsId']];
 	        }
 	    }
+	    $this->columnsArrayList = $tree;
 	    return $tree;
 	}	
-/*
-$items = array(
-    1 => array('id' => 1, 'pid' => 0, 'name' => '安徽省'),
-    2 => array('id' => 2, 'pid' => 0, 'name' => '浙江省'),
-    3 => array('id' => 3, 'pid' => 1, 'name' => '合肥市'),
-    4 => array('id' => 4, 'pid' => 3, 'name' => '长丰县'),
-    5 => array('id' => 5, 'pid' => 1, 'name' => '安庆市'),
-);
-print_r(generateTree($items));
-
-	 */
-	public function get_array ($id = 0) {
-		$arr = array();
-		$result = $this->getChildById($id);
-		if ($result && !empty($result)) {
-			$rows = array();
-			$arrayCount = count($result);
-			$x = 0;
-			while ($x < $arrayCount) {
-				$rows = $result[$x];
-				if (is_array($this->get_array($rows['columnsId']))) {
-					$rows['list'][] = &$this->get_array($rows['columnsId']);
-				} else {
-					$rows[] = &$this->get_array($rows['columnsId']);
-				}
-				$arr[] = $rows;
-				$x++;
-			}
-			return $arr;
-		} 
-	}
-
-	public function addItem($item) {
-		$count = count($this->columnsArrayList);
-		$this->columnsArrayList[$count] = $item;
-	}
-
-	public function getArrayList() {
-		return $this->columnsArrayList;
-	}
 
 }
 ?>
--- application/helpers/test.php
+++ application/helpers/test.php
@@ -3,19 +3,17 @@ include "ColumnsManager.php";
 
 $manager = new ColumnsManager;
 $arrayData = array(
-	array("columnsId" => 1, "columnsName" => "root"  , "parentId" => 0),
-	array("columnsId" => 2, "columnsName" => "leaf_1", "parentId" => 1),
-	array("columnsId" => 3, "columnsName" => "leaf_2", "parentId" => 1),
-	array("columnsId" => 4, "columnsName" => "leaf_3", "parentId" => 2),
-	array("columnsId" => 5, "columnsName" => "leaf_4", "parentId" => 2),
-	array("columnsId" => 6, "columnsName" => "leaf_5", "parentId" => 3),
-	array("columnsId" => 7, "columnsName" => "leaf_5", "parentId" => 3),
-	array("columnsId" => 8, "columnsName" => "leaf_5", "parentId" => 3),
-	array("columnsId" => 9, "columnsName" => "leaf_5", "parentId" => 4),
+	1 => array("columnsId" => 1, "columnsName" => "root"  , "parentId" => 0),
+	2 => array("columnsId" => 2, "columnsName" => "leaf_1", "parentId" => 1),
+	3 => array("columnsId" => 3, "columnsName" => "leaf_2", "parentId" => 1),
+	4 => array("columnsId" => 4, "columnsName" => "leaf_3", "parentId" => 2),
+	5 => array("columnsId" => 5, "columnsName" => "leaf_4", "parentId" => 2),
+	6 => array("columnsId" => 6, "columnsName" => "leaf_5", "parentId" => 3),
+	7 => array("columnsId" => 7, "columnsName" => "leaf_5", "parentId" => 3),
+	8 => array("columnsId" => 8, "columnsName" => "leaf_5", "parentId" => 3),
+	9 => array("columnsId" => 9, "columnsName" => "leaf_5", "parentId" => 4),
 	);
-$manager->init($arrayData);
-$manager->printColumnsArray();
-print_r($manager->generateTree($manager->getArrayList()));
-// $manager->getTreeByParentId(0);
-	// echo "php build success!!!";
+
+print_r($manager->generateTree($arrayData));
+
 ?>
